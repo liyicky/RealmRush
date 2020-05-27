a38d8b2292e1b34771f72a9ea8f73f6c537f822d
liyicky <liyicky@gmail.com>
2020-05-27 23:47:46 +0900

    editor snapping

--- Assets/EditorSnap.cs
+++ Assets/EditorSnap.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
+[SelectionBase]
 
 public class EditorSnap : MonoBehaviour
 {
+
     [SerializeField] [Range(1f, 20f)] float gridSize;
+
+    TextMesh textMesh;
+
+    private void Start()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -15,5 +24,8 @@ public class EditorSnap : MonoBehaviour
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
     }
 }
